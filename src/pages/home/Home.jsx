9b595dcf8f47c6721addf1b308d40cec0c2e3cf5
vishal-bashetti <vishalabashetti@gmail.com>
2021-09-05 20:45:46 +0530

    added chart from 'recharts'

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import './home.css'
 export default function home() {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart/>
         </div>
     )
 }
